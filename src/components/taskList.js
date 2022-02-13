d8d8a0f4b587b91bd76ede1439cfe14ab1bf2fc3
caglar.durmus <caglardur@gmail.com>
2022-02-13 18:05:01 +0300

    task list pagination added

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -1,14 +1,30 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import SingleTask from "./singleTask"
 
 const TaskList = ({ tasks }) => {
   const [showDetail, setShowDetail] = useState(null)
+  const [nowPage, setNowPage] = useState(0)
+  const [sliceTaskList, setSliceTaskList] = useState(tasks.slice(nowPage * 10, nowPage * 10 + 10))
+  const [pages, setPages] = useState(Math.ceil(tasks.length / 10))
+
+  console.log(tasks)
+
+  useEffect(() => {
+    setSliceTaskList(tasks.slice(nowPage * 10, nowPage * 10 + 10))
+    setPages(Math.ceil(tasks.length / 10))
+  }, [nowPage, tasks])
+
+  const pageArray = []
+  for (let int = 1; int <= pages; int++) {
+    pageArray.push(int)
+  }
+
   return (
     <div className="col">
-      {tasks ? (
-        tasks.length > 0 ? (
-          tasks.map((task, index) => (
+      {sliceTaskList ? (
+        sliceTaskList.length > 0 ? (
+          sliceTaskList.map((task, index) => (
             <div className="col m-2 p-2 shadow-sm bg-light" key={index}>
               <SingleTask task={task} showDetail={showDetail} setShowDetail={setShowDetail} />
             </div>
@@ -23,6 +39,26 @@ const TaskList = ({ tasks }) => {
           </div>
         </div>
       )}
+      <div className="col d-flex justify-content-center my-2">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination justify-content-center" id="pages">
+            {pages > 1 &&
+              pageArray.map(pg => (
+                <li key={pg} className={pg - 1 === nowPage ? "page-item active" : "page-item"} style={{ cursor: "pointer" }}>
+                  <div
+                    className="page-link"
+                    onClick={() => {
+                      setShowDetail(null)
+                      setNowPage(pg - 1)
+                    }}
+                  >
+                    {pg}
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
